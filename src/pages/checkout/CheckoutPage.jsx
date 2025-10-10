3bc1c675a157af148109bfb788ea49b32673d87b
hurstin <132925282+hurstin@users.noreply.github.com>
2025-10-10 21:01:06 +0100

    make creare order button work

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -59,7 +59,7 @@ const CheckoutPage = ({ cart, loadCart }) => {
             loadCart={loadCart}
           />
 
-          <PaymentSummary paymentSummary={paymentSummary} />
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
         </div>
       </div>
     </>
